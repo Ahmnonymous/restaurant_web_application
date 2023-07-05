b2e99e1a2022f6961d5a234b5ecba74e264a6d00
ahmnonymous <alihaiderawan1345@gmail.com>
2023-07-05 21:55:25 +0500

    cart updatess

--- js/script.js
+++ js/script.js
@@ -1,4 +1,68 @@
-// Open the login overlay when the user icon is clicked
+
+  const menuButtons = document.querySelectorAll('.menu-btn');
+
+  // Add click event listener to each menu button
+  menuButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      // Get the category from the data-category attribute
+      const category = button.getAttribute('data-category');
+
+      // Hide all food items
+      const foodItems = document.querySelectorAll('.food-item');
+      foodItems.forEach(item => {
+        item.style.display = 'none';
+      });
+
+      // Show food items of the selected category
+      if (category === 'all') {
+        // Show all food items
+        foodItems.forEach(item => {
+          item.style.display = 'block';
+        });
+      } else {
+        // Show food items of the selected category
+        const selectedFoodItems = document.querySelectorAll(`.${category}`);
+        selectedFoodItems.forEach(item => {
+          item.style.display = 'block';
+        });
+      }
+
+      // Remove the 'active' class from all buttons
+      menuButtons.forEach(btn => {
+        btn.classList.remove('active');
+      });
+
+      // Add the 'active' class to the clicked button
+      button.classList.add('active');
+    });
+  });
+
+
+  // Get the cart icon element and initialize the item count
+  const carticon = document.getElementById('cart-icon');
+  let itemcount = 0;
+
+  // Get all the "Add to Cart" buttons
+  const addToCartButtons = document.querySelectorAll('.card-detail .btn');
+
+  // Add event listener to each "Add to Cart" button
+  addToCartButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      // Increment the item count
+      itemcount++;
+
+      // Update the cart icon text with the new item count
+      carticon.innerText = itemcount;
+    });
+  });
+
+
+
+
+
+
+
+  // Open the login overlay when the user icon is clicked
 var userIcon = document.querySelector('.login-icon');
 var loginOverlay = document.querySelector('#login-overlay');
 
@@ -91,11 +155,31 @@ document.getElementById("card-option").addEventListener("change", function() {
 
 
 
+// Cart
+// Get the cart icon element and initialize the item count
+// Get the cart icon element and initialize the item count
+const addToCartButton = document.querySelectorAll('.add-to-cart-btn');
+  const sideCart = document.querySelector('.side-cart');
+  const cartItems = document.getElementById('cart-items');
+  let cartItemCount = 0;
 
+  addToCartButton.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      event.preventDefault();
+      const item = button.parentNode;
+      addToCart(item);
+    });
+  });
 
+  function addToCart(item) {
+    const cartItem = item.cloneNode(true);
+    cartItems.appendChild(cartItem);
+    cartItemCount++;
+    updateCartItemCount();
+    sideCart.classList.add('active');
+  }
 
-
-
-
-
-
+  function updateCartItemCount() {
+    const cartItemCountElement = document.querySelector('.cart-icon');
+    cartItemCountElement.textContent = cartItemCount;
+  }
