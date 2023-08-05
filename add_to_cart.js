5d0a654a642862ab2cfa8688d2cb28d7e2b94637
ahmnonymous <a4medqureshi8@gmail.com>
2023-08-06 01:23:12 +0500

    Updates by Ahmed -6/Aug/2023

---
+++ add_to_cart.js
@@ -0,0 +1,174 @@
+let cartItems = {}; 
+const checkedValues = [];
+
+function handleCheckboxChange(event) {
+  if (event.target.type === 'checkbox') {
+    if (event.target.checked) {
+      const value = event.target.value;
+      if (!checkedValues.includes(value)) {
+        checkedValues.push(value); // Push the value into the checkedValues array if not already present
+      }
+    } else {
+      const value = event.target.value;
+      const index = checkedValues.indexOf(value);
+      if (index !== -1) {
+        checkedValues.splice(index, 1); // Remove the value from checkedValues array if unchecked
+      }
+    }
+    console.log(checkedValues); // Log the array with checked values
+  }
+}
+
+function update(){
+const checkboxes = document.querySelectorAll('.checkbox-options input[type="checkbox"]');
+checkboxes.forEach(checkbox => {
+checkbox.addEventListener('change', handleCheckboxChange);
+});
+
+}
+
+function updateQuantityAndPrice(itemId, price) {
+  const quantityElement = document.querySelector("#overlay-" + itemId + " .quantity");
+  const priceElement = document.querySelector("#overlay-" + itemId + " .price");
+
+  if (quantityElement && priceElement) {
+    quantityElement.textContent = quantity; 
+    const basePrice = quantity * price;
+    const addonsPrice = calculateAddonsPrice(itemId);
+    const totalPrice = (basePrice + addonsPrice).toFixed(2);
+    priceElement.textContent = totalPrice;
+  }
+}
+
+
+function showOverlay(event,itemId) {
+  event.preventDefault();
+  const itemElement = event.target.closest('.dashboard-card');
+  document.getElementById("overlay-" + itemId).style.display = "block";
+  update();
+}
+
+function closeOverlay(event,itemId) {
+  event.preventDefault();
+  document.getElementById("overlay-" + itemId).style.display = "none";
+}
+
+function addToCart(event, itemId) {
+  event.preventDefault();
+
+  const quantityElement = document.querySelector("#overlay-" + itemId + " .quantity");
+  const quantity = parseInt(quantityElement.textContent);
+
+  const alertMessage = createAlertMessage('Item added to cart!');
+  displayAlertMessage(alertMessage);
+
+  addToCartItems(itemId);
+  updateCartCount();
+
+  const checkedValuesInt = checkedValues.map(value => parseInt(value)).filter(value => !isNaN(value));
+
+  // If checkedValues array is not empty, send separate AJAX requests for each value
+  if (checkedValuesInt && checkedValuesInt.length > 0) {
+    checkedValuesInt.forEach(value => {
+      // Send the data to the server via AJAX
+      const data = {
+        itemId: itemId,
+        quantity: quantity,
+        checkedValues: [value], // Pass the value in an array
+      };
+
+      const xhr = new XMLHttpRequest();
+      xhr.open('POST', './cpp/cart.cgi', true);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+          if (xhr.status === 200) {
+            const response = JSON.parse(xhr.responseText);
+            console.log(response);
+          } else {
+            console.error('Error:', xhr.status, xhr.statusText);
+          }
+        }
+      };
+      xhr.send(JSON.stringify(data));
+      checkedValues.length = 0;
+    });
+  } else {
+    // If checkedValues array is empty, send a single AJAX request with an empty array for checkedValues
+    const data = {
+      itemId: itemId,
+      quantity: quantity,
+      checkedValues: [], // Empty array to indicate no toppings selected
+    };
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', './cpp/cart.cgi', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+          const response = JSON.parse(xhr.responseText);
+          console.log(response);
+        } else {
+          console.error('Error:', xhr.status, xhr.statusText);
+        }
+      }
+    };
+    xhr.send(JSON.stringify(data));
+    checkedValues.length = 0;
+  }
+}
+
+function createAlertMessage(message) {
+  const alertMessage = document.createElement('div');
+  alertMessage.className = 'alert';
+  alertMessage.textContent = message;
+  return alertMessage;
+}
+
+function displayAlertMessage(alertMessage) {
+  document.body.appendChild(alertMessage);
+  setTimeout(function() {
+    alertMessage.style.display = 'none';
+  }, 2000);
+}
+
+function addToCartItems(itemId) {
+  if (!cartItems.hasOwnProperty(itemId)) {
+    cartItems[itemId] = 1;
+  }
+}
+
+function updateCartCount() {
+  const cartCount = Object.values(cartItems).reduce((total, count) => total + count, 0);
+  document.getElementById("cartCount").innerText = cartCount;
+}
+
+
+function calculateAddonsPrice(itemId) {
+  const checkboxes = document.querySelectorAll("#overlay-" + itemId+" .checkbox-options input[type=checkbox]");
+  let addonsPrice = 0;
+
+  checkboxes.forEach((checkbox) => {
+    if (checkbox.checked) {
+      const price = parseFloat(checkbox.nextElementSibling.nextElementSibling.textContent);
+      addonsPrice += price;
+    }
+  });
+
+  return addonsPrice;
+}
+
+let quantity = 1;
+
+  function incrementQuantity(itemId,price) {
+    quantity++;
+    updateQuantityAndPrice(itemId,price);
+  }
+
+  function decrementQuantity(itemId,price) {
+    if (quantity > 1) {
+      quantity--;
+      updateQuantityAndPrice(itemId,price);
+    }
+  }
--- database/rwa_db.sql
+++ database/rwa_db.sql
@@ -9,13 +9,14 @@ CREATE TABLE IF NOT EXISTS Customer (
   password VARCHAR(255) NOT NULL,
   phone VARCHAR(20) NOT NULL,
   is_auth TINYINT(1) DEFAULT 0
-);
+) AUTO_INCREMENT = 501;
 
 -- Create the AuthCode table
 CREATE TABLE IF NOT EXISTS AuthCode (
   id INT AUTO_INCREMENT PRIMARY KEY,
   customer_id INT,
   code VARCHAR(6) NOT NULL,
+  expiration_time DATETIME NOT NULL,
   FOREIGN KEY (customer_id) REFERENCES Customer(id)
 );
 
@@ -113,7 +114,7 @@ CREATE TABLE IF NOT EXISTS Employee (
   phone VARCHAR(20) NOT NULL,
   password VARCHAR(255) NOT NULL,
   privilege VARCHAR(20)
-);
+) AUTO_INCREMENT = 101;
 
 -- Create the Driver table
 CREATE TABLE IF NOT EXISTS Driver (
@@ -125,6 +126,15 @@ CREATE TABLE IF NOT EXISTS Driver (
   is_accepted TINYINT(1) DEFAULT 0,
   salary DECIMAL(10,2),
   working_time VARCHAR(255)
+) AUTO_INCREMENT = 201;
+
+-- Create the Item_Topping table
+CREATE TABLE IF NOT EXISTS Item_Topping (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  item_id INT,
+  topping_id INT,
+  FOREIGN KEY (item_id) REFERENCES Item(id),
+  FOREIGN KEY (topping_id) REFERENCES Topping(id)
 );
 
 -- Create the Cart table
@@ -132,12 +142,12 @@ CREATE TABLE IF NOT EXISTS Cart (
   id INT AUTO_INCREMENT PRIMARY KEY,
   customer_id INT,
   item_id INT,
-  topping_id INT,
+  item_topping_id INT,
   quantity INT,
   creation_date DATETIME,
   FOREIGN KEY (customer_id) REFERENCES Customer(id),
   FOREIGN KEY (item_id) REFERENCES Item(id),
-  FOREIGN KEY (topping_id) REFERENCES Topping(id)
+  FOREIGN KEY (item_topping_id) REFERENCES Item_Topping(id)
 );
 
 -- Create the PastOrders table
@@ -170,6 +180,30 @@ CREATE TABLE IF NOT EXISTS Rating (
   FOREIGN KEY (order_id) REFERENCES `Order`(id)
 );
 
+-- Create the cartd view
+CREATE VIEW cartd AS
+SELECT
+    cu.name AS customer_name,
+    c.customer_id,
+    c.item_id,
+    i.name AS item_name,
+    i.price AS item_price,
+    c.item_topping_id,
+    t.name AS topping_name,
+    t.price AS topping_price
+FROM
+    Cart c
+JOIN
+    Customer cu ON c.customer_id = cu.id
+JOIN
+    Item i ON c.item_id = i.id
+LEFT JOIN
+    Item_Topping it ON c.item_topping_id = it.id
+LEFT JOIN
+    Topping t ON it.topping_id = t.id;
+
+
+-- Insert data into the Menu table
 INSERT INTO Menu (category)
 VALUES
   ('Platters'),
@@ -177,6 +211,7 @@ VALUES
   ('Sweets'),
   ('Beverage');
 
+-- Insert data into the Item table
 INSERT INTO Item (menu_id, name, description, price, picture)
 VALUES
   (1, 'Foul Plate', 'Delicious item to order', 10.00, 'burger.jpg'),
@@ -202,6 +237,7 @@ VALUES
   (4, 'Milk', 'Delicious item to order', 12.00, 'burger.jpg'),
   (4, 'Water', 'Delicious item to order', 1.00, 'burger.jpg');
 
+-- Insert data into the Topping table
 INSERT INTO Topping (item_id, name, price)
 VALUES
   (8, 'Nutella', 1.50),
@@ -213,3 +249,34 @@ VALUES
   (9, 'Chia Seeds', 0.25),
   (9, 'Powdered Sugar', 0.25),
   (9, 'Custard', 0.75);
+
+-- Insert data into the Item_Topping table
+INSERT INTO Item_Topping (item_id, topping_id)
+VALUES
+  (8, 1),   -- Foul Sandwich with Nutella
+  (8, 2),   -- Foul Sandwich with Lotus
+  (8, 3),   -- Foul Sandwich with Peanut Butter
+  (9, 4),   -- Spicy Foul Sandwich with Greek Yogurt
+  (9, 5),   -- Spicy Foul Sandwich with Fruits
+  (9, 6),   -- Spicy Foul Sandwich with Nuts
+  (9, 7),   -- Spicy Foul Sandwich with Chia Seeds
+  (9, 8),   -- Spicy Foul Sandwich with Powdered Sugar
+  (9, 9);   -- Spicy Foul Sandwich with Custard
+
+-- Insert data into the Customer, Address, Employee, and Driver tables
+INSERT INTO Customer (name, password, phone, is_auth)
+VALUES
+  ('John Doe', 'password123', '+123456789', 1);
+
+INSERT INTO Address (customer_id, area, building_number, apartment_number)
+VALUES
+  (501, 'Main Street', '123', 'Apt 5B');
+
+INSERT INTO Employee (name, phone, password, privilege)
+VALUES
+  ('Admin User', '+987654321', 'adminpassword', 'admin'),
+  ('Regular User', '+555555555', 'userpassword', 'user');
+
+INSERT INTO Driver (name, license_number, vehicle_information, password, is_accepted, salary, working_time)
+VALUES
+  ('Driver User', 'DL123456', 'Bike', 'driverpassword', 1, 2000.00, 'Full Time');
